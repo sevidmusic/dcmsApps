97752328cbd8653ba65ce8af96a9bffc3ea92b48
sevidmusic <sevidmusic@gmail.com>
2018-12-02 16:46:37 -0500

    Earnings Invoice view can now show selected ranges of time cards

--- Earnings/views/Invoice.php
+++ Earnings/views/Invoice.php
@@ -1,17 +1,19 @@
 <?php
 if (filter_input(INPUT_GET, 'ajaxRequest') === 'true') {
     require '../../../vendor/autoload.php';
-    require  '../functions.php';
+    require '../functions.php';
 }
 $timeCard = new \Apps\Earnings\classes\TimeCard('now', new DateTimeZone('America/New_York'));
 $timeCardCalculator = new \Apps\Earnings\classes\TimeCardCalculator($timeCard);
+$userInterface = new \Apps\Earnings\classes\EarningsUI($timeCard);
 $inOut = '';
-$timeCardNames = $timeCard->getTimeCardNames();
+$timeCardNames = $userInterface->getTimeCardNames();
 $firstTimeCardName = array_shift($timeCardNames);
 $lastTimeCardName = array_pop($timeCardNames);
-$totalHoursWorked = $timeCardCalculator->calculateTimeWorked('hours', array(\Apps\Earnings\classes\TimeCardCalculator::OPTION_RANGE => array($firstTimeCardName, $lastTimeCardName)));
+$totalHoursWorked = $userInterface->geTimeWorkedFromSelected();
 ?>
 <h1>Invoice</h1>
+<?php echo $userInterface->getTimeCardRangeSelector(); ?>
 <p>Total Hours Worked: <?php echo $totalHoursWorked; ?></p>
 <p>Total Money Earned (Due): $<?php echo calculateEarnings($totalHoursWorked, '10'); ?></p>
 <p>Total Money Earned Toward Debt: $<?php echo calculateEarnings($totalHoursWorked, '2.25'); ?></p>
@@ -23,7 +25,7 @@ $totalHoursWorked = $timeCardCalculator->calculateTimeWorked('hours', array(\App
         <th class="timeCard-invoice-table-th">Money Earned On This Date</th>
         <th class="timeCard-invoice-table-th">Money Earned Toward Debt On This Date</th>
     </tr>
-    <?php foreach ($timeCard->getTimeCardNames() as $timeCardName) { ?>
+    <?php foreach ($userInterface->getTimeCardNames() as $timeCardName) { ?>
         <tr class="timeCard-invoice-table-tr">
             <td class="timeCard-invoice-table-td timeCard-invoice-table-date-col">
                 <?php echo "{$timeCardName[0]}{$timeCardName[1]}/{$timeCardName[2]}{$timeCardName[3]}/{$timeCardName[4]}{$timeCardName[5]}{$timeCardName[6]}{$timeCardName[7]}"; ?>
--- Earnings/views/dev.php
+++ Earnings/views/dev.php
@@ -1,1 +1,60 @@
 <h1>Dev</h1>
+<?php
+if (filter_input(INPUT_GET, 'ajaxRequest') === 'true') {
+    require '../../../vendor/autoload.php';
+    require '../functions.php';
+}
+$timeCard = new \Apps\Earnings\classes\TimeCard('now', new DateTimeZone('America/New_York'));
+$timeCardCalculator = new \Apps\Earnings\classes\TimeCardCalculator($timeCard);
+$userInterface = new \Apps\Earnings\classes\EarningsUI($timeCard);
+$inOut = '';
+$timeCardNames = $userInterface->getTimeCardNames();
+$firstTimeCardName = array_shift($timeCardNames);
+$lastTimeCardName = array_pop($timeCardNames);
+$totalHoursWorked = $userInterface->geTimeWorkedFromSelected();
+?>
+<h1>Invoice</h1>
+<?php echo $userInterface->getTimeCardRangeSelector(); ?>
+<p>Total Hours Worked: <?php echo $totalHoursWorked; ?></p>
+<p>Total Money Earned (Due): $<?php echo calculateEarnings($totalHoursWorked, '10'); ?></p>
+<p>Total Money Earned Toward Debt: $<?php echo calculateEarnings($totalHoursWorked, '2.25'); ?></p>
+<table class="timeCard-invoice-table">
+    <tr class="timeCard-invoice-table-tr">
+        <th class="timeCard-invoice-table-th">Date</th>
+        <th class="timeCard-invoice-table-th">Punches</th>
+        <th class="timeCard-invoice-table-th">Total Time Worked</th>
+        <th class="timeCard-invoice-table-th">Money Earned On This Date</th>
+        <th class="timeCard-invoice-table-th">Money Earned Toward Debt On This Date</th>
+    </tr>
+    <?php foreach ($userInterface->getTimeCardNames() as $timeCardName) { ?>
+        <tr class="timeCard-invoice-table-tr">
+            <td class="timeCard-invoice-table-td timeCard-invoice-table-date-col">
+                <?php echo "{$timeCardName[0]}{$timeCardName[1]}/{$timeCardName[2]}{$timeCardName[3]}/{$timeCardName[4]}{$timeCardName[5]}{$timeCardName[6]}{$timeCardName[7]}"; ?>
+            </td>
+            <td class="timeCard-invoice-table-td timeCard-invoice-table-punches-col">
+                <ul>
+                    <?php
+                    foreach ($timeCard->getTimeCardData($timeCardName) as $punch) {
+                        $inOut = ($inOut === 'Punch In' ? 'Punch Out' : 'Punch In');
+                        echo '<li>' . $timeCard->timestampToString($punch) . ' ' . $inOut . '</li>';
+                    }
+                    ?>
+                </ul>
+            </td>
+            <td class="timeCard-invoice-table-td timeCard-invoice-table-timeWorked-col">
+                <ul>
+                    <li>Hours: <?php echo ' ' . $timeCardCalculator->calculateTimeCard($timeCardName, 'hours'); ?></li>
+                    <li>Total
+                        Time: <?php echo \Apps\Earnings\classes\EarningsUI::formatTimeForDisplay($timeCardCalculator->calculateTimeCard($timeCardName, 'hours'), $timeCardCalculator->calculateTimeCard($timeCardName, 'minutes')); ?></li>
+                </ul>
+            </td>
+            <td class="timeCard-invoice-table-td timeCard-invoice-table-moneyEarned-col">
+                <?php echo '$' . calculateEarnings($timeCardCalculator->calculateTimeCard($timeCardName, 'hours'), '10'); ?>
+            </td>
+            <td class="timeCard-invoice-table-td timeCard-invoice-table-moneyEarnedTowardDebt-col">
+                <?php echo '$' . calculateEarnings($timeCardCalculator->calculateTimeCard($timeCardName, 'hours'), '2.25'); ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
