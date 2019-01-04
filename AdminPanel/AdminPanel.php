81d04371f9122645005bacc42ced13a4b685ee77
sevidmusic <sevidmusic@gmail.com>
2019-01-03 23:40:32 -0500

    various changes

--- AdminPanel/AdminPanel.php
+++ AdminPanel/AdminPanel.php
@@ -2,23 +2,18 @@
     <button title="Show all admin panels" id="admin-panel-show-all-link">Show Admin Panels</button>
     <button title="Hide all admin panels" id="admin-panel-hide-all-link">Hide Admin Panels</button>
     <button title="Hint: Click to show | Double click to hide..." onclick="showAppPanel('AppManager')"
-            ondblclick="hideAppPanel('AppManager')"
-            id="admin-panel-show-APPNAME-link">App Manager
+            ondblclick="hideAppPanel('AppManager')">App Manager
     </button>
     <button title="Hint: Click to show | Double click to hide..." onclick="showAppPanel('Earnings')"
-            ondblclick="hideAppPanel('Earnings')" id="admin-panel-show-APPNAME-link">
-        Earnings
+            ondblclick="hideAppPanel('Earnings')">Earnings
     </button>
     <button title="Hint: Click to show | Double click to hide..." onclick="showAppPanel('PHPMyAdminFrame')"
-            ondblclick="hideAppPanel('PHPMyAdminFrame')"
-            id="admin-panel-show-APPNAME-link">PHP My Admin Frame
+            ondblclick="hideAppPanel('PHPMyAdminFrame')">PHP My Admin Frame
     </button>
     <button title="Hint: Click to show | Double click to hide..." onclick="showAppPanel('ActionManager')"
-            ondblclick="hideAppPanel('ActionManager')"
-            id="admin-panel-show-APPNAME-link">Action Manager
+            ondblclick="hideAppPanel('ActionManager')">Action Manager
     </button>
     <button title="Hint: Click to show | Double click to hide..." onclick="showAppPanel('Todo')"
-            ondblclick="hideAppPanel('Todo')"
-            id="admin-panel-show-APPNAME-link">Todo
+            ondblclick="hideAppPanel('Todo')">Todo
     </button>
 </div>
