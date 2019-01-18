068aa31ee41282a6d68ccf8d959d847deeeb7944
sevidmusic <sevidmusic@gmail.com>
2019-01-18 02:24:11 -0500

    Permission Manager: managerPermissions is working.

--- PermissionManager/handlers/updatePermissionHandler.php
+++ PermissionManager/handlers/updatePermissionHandler.php
@@ -1,4 +1,29 @@
 <?php
-var_dump(filter_input_array(INPUT_POST));
+
+use \DarlingCms\classes\staticClasses\core\CoreValues;
+
+if (filter_input(INPUT_POST, 'ajaxRequest') === 'true') {
+    require str_replace('/apps/PermissionManager/handlers', '/vendor/autoload.php', __DIR__);
+}
+
+$sqlQuery = CoreValues::getISqlQueryInstance
+(
+    CoreValues::CORE_DB_HOST,
+    CoreValues::CORE_DB_NAME,
+    'root',
+    'root'
+);
+$actionCrud = new \DarlingCms\classes\crud\MySqlActionCrud($sqlQuery);
+$permissionCrud = new \DarlingCms\classes\crud\MySqlPermissionCrud($sqlQuery, $actionCrud);
+$post = filter_input_array(INPUT_POST);
+$actions = array_merge(array_combine($post['assignedActionNames'], $post['assignedActionStates']), array_combine($post['unAssignedActionNames'], $post['unAssignedActionStates']));
+$assignedActions = array();
+foreach ($actions as $actionName => $actionState) {
+    if ($actionState === 'true') {
+        array_push($assignedActions, $actionCrud->read($actionName));
+    }
+}
+$newPermission = new \DarlingCms\classes\privilege\Permission($post['permissionName'], $assignedActions);
+var_dump($permissionCrud->update($post['originalPermissionName'], $newPermission));
 ?>
 <p>This will update permissions...</p>
