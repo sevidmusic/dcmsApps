f40c7e6275fecee762f1d7927962ab9a899493d3
sevidmusic <sevidmusic@gmail.com>
2019-02-01 10:55:35 -0500

    Role Manager is now working as designed. Roles can now be created, updated, and deleted from UI, and their permissions can now be set upon creation of new roles.

--- RoleManager/handlers/createRoleHandler.php
+++ RoleManager/handlers/createRoleHandler.php
@@ -17,12 +17,25 @@ $actionCrud = new \DarlingCms\classes\crud\MySqlActionCrud($sqlQuery);
 $permissionCrud = new \DarlingCms\classes\crud\MySqlPermissionCrud($sqlQuery, $actionCrud);
 $roleCrud = new \DarlingCms\classes\crud\MySqlRoleCrud($sqlQuery, $permissionCrud);
 $post = filter_input_array(INPUT_POST);
-$newRole = new \DarlingCms\classes\privilege\Role($post['roleName'], array());
-var_dump($post);
-/*
-if ($roleCrud->create($newRole) === true) {
-echo '<p class="dcms-positive-text">Created the ' . $post['roleName'] . ' Role Successfully...</p>';
-} else {
-echo '<p class="dcms-negative-text">The ' . $post['roleName'] . ' Role could not be created. Please try again...</p>';
+$permissions = array_merge(
+    array_combine(
+        (isset($post['assignedPermissionNames']) ? $post['assignedPermissionNames'] : array()),
+        (isset($post['assignedPermissionStates']) ? $post['assignedPermissionStates'] : array())
+    ),
+    array_combine(
+        (isset($post['unAssignedPermissionNames']) ? $post['unAssignedPermissionNames'] : array()),
+        (isset($post['unAssignedPermissionStates']) ? $post['unAssignedPermissionStates'] : array())
+    )
+);
+$assignedPermissions = array();
+foreach ($permissions as $permissionName => $permissionState) {
+    if ($permissionState === 'true') {
+        array_push($assignedPermissions, $permissionCrud->read($permissionName));
+    }
 }
-*/
+$newRole = new \DarlingCms\classes\privilege\Role($post['roleName'], $assignedPermissions);
+if ($roleCrud->create($newRole) === true) {
+    echo '<p class="dcms-positive-text">Created the new ' . $post['roleName'] . ' Role Successfully...</p>';
+} else {
+    echo '<p class="dcms-negative-text">The ' . $post['roleName'] . ' Role could not be created. Please try again...</p>';
+}
