fa2ebf45a1c823e7a771c1ec51f2ade8fb920d9a
sevidmusic <sevidmusic@gmail.com>
2019-02-03 19:40:19 -0500

    Various changes, Users can now be deleted, as well as updated, via the User Manager UI

--- UserManager/handlers/deleteUserHandler.php
+++ UserManager/handlers/deleteUserHandler.php
@@ -3,7 +3,7 @@
 use \DarlingCms\classes\staticClasses\core\CoreValues;
 
 if (filter_input(INPUT_POST, 'ajaxRequest') === 'true') {
-    require str_replace('/apps/RoleManager/handlers', '/vendor/autoload.php', __DIR__);
+    require str_replace('/apps/UserManager/handlers', '/vendor/autoload.php', __DIR__);
 }
 
 $sqlQuery = CoreValues::getISqlQueryInstance
@@ -16,9 +16,10 @@ $sqlQuery = CoreValues::getISqlQueryInstance
 $actionCrud = new \DarlingCms\classes\crud\MySqlActionCrud($sqlQuery);
 $permissionCrud = new \DarlingCms\classes\crud\MySqlPermissionCrud($sqlQuery, $actionCrud);
 $roleCrud = new \DarlingCms\classes\crud\MySqlRoleCrud($sqlQuery, $permissionCrud);
+$userCrud = new \DarlingCms\classes\crud\MySqlUserCrud($sqlQuery, $roleCrud);
 $post = filter_input_array(INPUT_POST);
-if ($roleCrud->delete($post['roleName']) == true) {
-    echo '<p class="dcms-positive-text">Deleted the ' . $post['roleName'] . ' Role Successfully...</p>';
+if ($userCrud->delete($post['userName']) == true) {
+    echo '<p class="dcms-positive-text">Deleted the ' . $post['userName'] . ' User Successfully...</p>';
 } else {
-    echo '<p class="dcms-negative-text">The ' . $post['roleName'] . ' Role could not be deleted. Please try again...</p>';
+    echo '<p class="dcms-negative-text">The ' . $post['userName'] . ' User could not be deleted. Please try again...</p>';
 }
