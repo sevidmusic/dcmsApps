ad2e43524e8544c4b7ec83400e484a2620adebf7
sevidmusic <sevidmusic@gmail.com>
2019-02-07 01:04:53 -0500

    Various changes, the Earnings app's punch.php file now correctyl uses composer's autoloader

--- Earnings/ajax/punch.php
+++ Earnings/ajax/punch.php
@@ -1,10 +1,8 @@
 <?php
-require realpath('../../../core/interfaces/userInterface/IUserInterface.php');
-require realpath('../classes/TimeCard.php');
-require realpath('../classes/TimeCardCalculator.php');
-require realpath('../classes/EarningsUI.php');
-require realpath('../functions.php');
-
+if (filter_input(INPUT_POST, 'ajaxRequest') === 'true') {
+    require str_replace('/apps/Earnings/ajax', '/vendor/autoload.php', __DIR__);
+    require '../functions.php';
+}
 $timeCard = new \Apps\Earnings\classes\TimeCard('now', new DateTimeZone('America/New_York'));
 $userInterface = new \Apps\Earnings\classes\EarningsUI($timeCard);
 echo str_replace(array('Click here to move', 'draggable="true"'), array('Drag disabled', 'draggable="false"'), $userInterface->getDragHandle()); // @todo: Drag does not work for post ajax request...fix!
@@ -14,7 +12,7 @@ echo $userInterface->getPunchButton();
 if ($punchStatus = $timeCard->punch($timeCard->getCurrentTimestamp()) === true) {
     echo '<p class="dcms-positive-text earnings-msg">Logged punch on ' . $timeCard->timestampToString($timeCard->getCurrentTimestamp()) . '</p>';
 }
-if($punchStatus === false){
+if ($punchStatus === false) {
     echo '<p class="dcms-negative-text earnings-msg">Failed to log punch on ' . $timeCard->timestampToString($timeCard->getCurrentTimestamp()) . '. Please try again.</p>';
 }
 echo $userInterface->getCurrentView();
