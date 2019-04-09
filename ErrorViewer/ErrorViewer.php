8b8056740ae5d485092ea5aed565e9abe19bc36f
sevidmusic <sevidmusic@gmail.com>
2019-04-08 20:55:37 -0400

    Moved ErrorViewer inline styles into theme.

--- ErrorViewer/ErrorViewer.php
+++ ErrorViewer/ErrorViewer.php
@@ -6,8 +6,7 @@ if (file_exists(ini_get('error_log'))) {
     $errorLog = explode(PHP_EOL, file_get_contents(ini_get('error_log')));
     if (((count($errorLog) <= 1 && count($errorLog) > 0) && $errorLog[0] === '') === false) {
         ?>
-        <div id="ErrorViewer" class="dcms-admin-panel dcms-make-draggable">
-            <div draggable="true" id="ErrorViewerHandle" class="dcms-drag-handle">Click here to move</div>
+        <div id="ErrorViewer" class="dcms-admin-panel">
             <a class="ev-link" href="index.php?ErrorViewerAction=ClearErrors&ErrorLog=default">Clear Errors</a>
             <div class="dcms-clear-float"></div>
             <?php
@@ -28,7 +27,7 @@ if (file_exists(ini_get('error_log'))) {
                 if (empty($item) === true) {
                     continue;
                 }
-                echo '<p class="error-viewer-error" style="opacity:.87; background-color:' . $bgColor . ';overflow: auto;border: 3px solid #ffffff; border-radius: 20px; padding: 15px;margin-bottom: 2%;">' . htmlentities($item) . '</p>';
+                echo '<p class="error-viewer-error" style="background-color:' . $bgColor . ';">' . htmlentities($item) . '</p>';
             }
             if (empty(filter_input(INPUT_GET, 'ErrorViewerAction')) === false && empty(filter_input(INPUT_GET, 'ErrorLog')) === false) {
                 $status = file_put_contents(ini_get('error_log'), '', LOCK_EX);
@@ -37,7 +36,7 @@ if (file_exists(ini_get('error_log'))) {
                         echo '<p class="dcms-negative-text ev-message">Failed to clear error log.</p>';
                         break;
                     default:
-                        echo '<p style="background: black; padding: 5px;" class="dcms-positive-text ev-message">Cleared error log.</p>';
+                        echo '<p class="dcms-positive-text ev-message">Cleared error log.</p>';
                         $rootUrl = CoreValues::getSiteRootUrl();
                         echo "<meta http-equiv='refresh' content='1;url={$rootUrl}' />";
                         break;
