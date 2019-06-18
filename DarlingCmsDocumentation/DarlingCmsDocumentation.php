a3ba6642e9b7d99a193955778e2bcc9cf99c70ef
sevidmusic <sevidmusic@gmail.com>
2019-06-18 15:08:15 -0400

    Various changes, added scroll behavior to DCMS Documentation App

--- DarlingCmsDocumentation/DarlingCmsDocumentation.php
+++ DarlingCmsDocumentation/DarlingCmsDocumentation.php
@@ -6,8 +6,10 @@ use DarlingCms\classes\staticClasses\core\CoreValues;
 require_once 'functions.php';
 ?>
 <div id="DarlingCmsDocumentation" class="dcms-admin-panel dcms-admin-panel-pos1 dcms-make-draggable todo-container">
+    <div id="DarlingCmsDocumentationTopPlaceholder"></div>
     <div id="DarlingCmsDocumentationHandle" draggable="true" class="dcms-drag-handle">Click here to drag...</div>
-    <div class="dcms-button" onclick="document.getElementById('DarlingCmsDocumentation').scrollTop=0;"
+    <div class="dcms-button"
+         onclick='document.getElementById("DarlingCmsDocumentationTopPlaceholder").scrollIntoView({behavior: "smooth"})'
          style="z-index:9000;background:#16232a;position: sticky;top:70px;padding: 20px; margin: 10px auto; width: 100%; text-align: center; opacity: .72;">
         Scroll To Top
     </div>
@@ -23,7 +25,8 @@ require_once 'functions.php';
     $docCommentMenu = '';
     foreach ($docComments as $name => $docComment) {
         $htmlId = sprintf('%s', str_replace(['\\'], [''], $name));
-        $docCommentMenu .= '<a  href="#' . $htmlId . '">' . $name . '</a><br/>';
+        // Use this if normal links are needed: $docCommentMenu .= '<a href="#' . $htmlId . '">' . $name . '</a><br/>';
+        $docCommentMenu .= '<a onclick=\'document.getElementById("' . $htmlId . '").scrollIntoView({behavior: "smooth"})\'>' . $name . '</a><br/>';
         if (empty($docComment) === true) {
             error_log('Darling Cms Documentation Notice: There are no doc comments defined for ' . $name);
             continue;
